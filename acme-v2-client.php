b94e55317539caf86ac0ff475424b08d6ecbb595
kaien <kaienkira@gmail.com>
2020-02-05 18:02:53 +0800

    work sync

--- acme-v2-client.php
+++ acme-v2-client.php
@@ -1,12 +1,227 @@
 #!/usr/bin/env php
 <?php
 
-class Config
+final class Config
 {
     // debug use
     public static $acme_url_base = 'https://acme-staging-v02.api.letsencrypt.org';
     // prod use
     // public static $acme_url_base = 'https://acme-v02.api.letsencrypt.org';
+}
+
+final class Util
+{
+    public static function urlbase64($bin)
+    {
+        return str_replace(
+            array('+', '/', '='),
+            array('-', '_', ''),
+            base64_encode($bin));
+    }
+
+    public static function signMessage($key, $message)
+    {
+        if (openssl_sign($message, $sign, $key, 'sha256') === false) {
+            return false;
+        }
+
+        return self::urlbase64($sign);
+    }
+
+    public static function getRsaKeyInfo($key)
+    {
+        $key_info = openssl_pkey_get_details($key);
+        if ($key_info === false) {
+            echo "openssl failed: ".openssl_error_string()."\n";
+            return false;
+        }
+        if (!isset($key_info['rsa'])) {
+            echo "account key file is not rsa private key\n";
+            return false;
+        }
+
+        return array(
+            'e' => self::urlbase64($key_info['rsa']['e']),
+            'n' => self::urlbase64($key_info['rsa']['n']),
+        );
+    }
+
+    public static function loadAccountKey($account_key_file)
+    {
+        $key_file_content = file_get_contents($account_key_file);
+        if ($key_file_content === false) {
+            echo "can not open file: $account_key_file\n";
+            return false;
+        }
+        $key = openssl_pkey_get_private($key_file_content);
+        if ($key === false) {
+            echo "openssl failed: ".openssl_error_string()."\n";
+            return false;
+        }
+
+        return $key;
+    }
+
+    public static function loadCsrFile($csr_file)
+    {
+        $csr_file_content = file_get_contents($csr_file);
+        if ($csr_file_content === false) {
+            echo "can not open file: $csr_file_content\n";
+            return false;
+        }
+        $lines = explode("\n", $csr_file_content);
+        unset($lines[0]);
+        unset($lines[count($lines) - 1]);
+        return urlbase64(base64_decode(implode('', $lines)));
+    }
+
+    public static function httpRequest($url, $method, $post_data = '')
+    {
+        $ch = curl_init();
+        curl_setopt($ch, CURLOPT_URL, $url);
+        curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+        curl_setopt($ch, CURLOPT_HEADER, true);
+
+        $method = strtolower($method);
+        if ($method === 'head') {
+            // head method
+            curl_setopt($ch, CURLOPT_NOBODY, true);
+        } else if ($method === 'get') {
+            // get method
+        } else if ($method === 'post') {
+            // post method
+            curl_setopt($ch, CURLOPT_POST, true);
+            curl_setopt($ch, CURLOPT_POSTFIELDS, $post_data);
+        } else {
+            echo "curl failed: invalid http method\n";
+            return false;
+        }
+
+        $output = curl_exec($ch);
+        if ($output === false) {
+            echo 'curl failed: '.curl_error($ch)."\n";
+            return false;
+        }
+        $http_code = curl_getinfo($ch, CURLINFO_HTTP_CODE);
+        $header_size = curl_getinfo($ch, CURLINFO_HEADER_SIZE);
+        $header = substr($output, 0, $header_size);
+        $response = substr($output, $header_size);
+
+        curl_close($ch);
+
+        return array(
+            'http_code' => $http_code,
+            'header' => $header,
+            'response' => $response,
+        );
+    }
+}
+
+final class AcmeClient
+{
+    private $account_key_info = null;
+    private $nonce = null;
+
+    private $new_nonce_url = null;
+    private $new_account_url = null;
+    private $new_order_url = null;
+    private $tos_url = null;
+
+    public function init($account_key)
+    {
+        $account_key_info = Util::getRsaKeyInfo($account_key);
+        if ($account_key_info === false) {
+            return false;
+        }
+        $this->account_key_info = $account_key_info;
+
+        $ret = httpRequest(Config::$acme_url_base.'/directory', 'get');
+        if ($ret === false) {
+            return false;
+        }
+
+        $response = json_decode($ret['response'], true);
+        if ($response === false) {
+            echo 'acme/directory failed: invalid response'."\n";
+            return false;
+        }
+
+        if (isset($response['newNonce']) === false) {
+            echo 'acme/directory failed: `newNonce` not found'."\n";
+            return false;
+        }
+        $this->new_nonce_url = $response['newNonce'];
+
+        if (isset($response['newAccount']) === false) {
+            echo 'acme/directory failed: `newAccount` not found'."\n";
+            return false;
+        }
+        $this->new_account_url = $response['newAccount'];
+
+        if (isset($response['newOrder']) === false) {
+            echo 'acme/directory failed: `newOrder` not found'."\n";
+            return false;
+        }
+        $this->new_order_url = $response['newOrder'];
+
+        if (isset($response['meta']) === false ||
+            isset($response['meta']['termsOfService']) === false) {
+            echo 'acme/directory failed: `meta/termsOfService` not found'."\n";
+            return false;
+        }
+        $this->tos_url = $response['meta']['termsOfService'];
+
+        return true;
+    }
+
+    private function checkTermOfService($tos)
+    {
+        if ($tos != '' && $tos != $this->tos_url) {
+            echo "terms of service has changed: ".
+                 "please modify your -t command option\n".
+                 'new tos: '.$this->tos_url."\n";
+            return false;
+        }
+
+        return true;
+    }
+
+    private function registerAccount()
+    {
+        // register account
+        $ret = signedHttpRequest($this->new_account_url, array(
+            'termsOfServiceAgreed' => true,
+        ));
+
+        return true;
+    }
+
+    private function getReplayNonce($http_response_header)
+    {
+        preg_match('/^[Rr]eplay-[Nn]once: (.*?)\r\n/sm',
+            $http_response_header, $matches);
+        if (isset($matches[1]) === false) {
+            echo "curl failed: replay nonce header is missing\n";
+            return false;
+        }
+        $this->nonce = $matches[1];
+
+        return true;
+    }
+
+    private function signedHttpRequest($url, $payload)
+    {
+        // get first nonce
+        if ($this->nonce === null) {
+            $ret = httpRequest($this->new_nonce_url, 'head');
+            if ($ret === false) {
+                return false;
+            }
+            if (getReplayNonce($ret['header']) === false) {
+                return false;
+            }
+        }
+    }
 }
 
 function printUsage($prog_name)
@@ -19,198 +234,6 @@ function printUsage($prog_name)
          '-o <output_cert_file>'.
          '[-t <terms_of_service>]'.
          "\n";
-}
-
-function urlbase64($bin)
-{
-    return str_replace(
-        array('+', '/', '='),
-        array('-', '_', ''),
-        base64_encode($bin));
-}
-
-function loadAccountKey($account_key_file)
-{
-    $key_file_content = file_get_contents($account_key_file);
-    if ($key_file_content === false) {
-        echo "can not open file: $account_key_file\n";
-        return false;
-    }
-    $key = openssl_pkey_get_private($key_file_content);
-    if ($key === false) {
-        echo "openssl failed: ".openssl_error_string()."\n";
-        return false;
-    }
-
-    return $key;
-}
-
-function loadCsrFile($csr_file)
-{
-    $csr_file_content = file_get_contents($csr_file);
-    if ($csr_file_content === false) {
-        echo "can not open file: $csr_file_content\n";
-        return false;
-    }
-    $lines = explode("\n", $csr_file_content);
-    unset($lines[0]);
-    unset($lines[count($lines) - 1]);
-    return urlbase64(base64_decode(implode('', $lines)));
-}
-
-function getRsaKeyInfo($key)
-{
-    $key_info = openssl_pkey_get_details($key);
-    if ($key_info === false) {
-        echo "openssl failed: ".openssl_error_string()."\n";
-        return false;
-    }
-    if (!isset($key_info['rsa'])) {
-        echo "account key file is not rsa private key\n";
-        return false;
-    }
-
-    return array(
-        'e' => urlbase64($key_info['rsa']['e']),
-        'n' => urlbase64($key_info['rsa']['n']),
-    );
-}
-
-function signMessage($key, $message)
-{
-    if (openssl_sign($message, $sign, $key, 'sha256') === false) {
-        return false;
-    }
-
-    return urlbase64($sign);
-}
-
-function httpRequest($url, $method, $post_data = '')
-{
-    $ch = curl_init();
-    curl_setopt($ch, CURLOPT_URL, $url);
-    curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
-    curl_setopt($ch, CURLOPT_HEADER, true);
-
-    $method = strtolower($method);
-    if ($method === 'head') {
-        // head method
-        curl_setopt($ch, CURLOPT_NOBODY, true);
-    } else if ($method === 'get') {
-        // get method
-    } else if ($method === 'post') {
-        // post method
-        curl_setopt($ch, CURLOPT_POST, true);
-        curl_setopt($ch, CURLOPT_POSTFIELDS, $post_data);
-    } else {
-        echo "curl failed: invalid http method\n";
-        return false;
-    }
-
-    $output = curl_exec($ch);
-    if ($output === false) {
-        echo 'curl failed: '.curl_error($ch)."\n";
-        return false;
-    }
-    $http_code = curl_getinfo($ch, CURLINFO_HTTP_CODE);
-    $header_size = curl_getinfo($ch, CURLINFO_HEADER_SIZE);
-    $header = substr($output, 0, $header_size);
-    $response = substr($output, $header_size);
-
-    curl_close($ch);
-
-    return array(
-        'http_code' => $http_code,
-        'header' => $header,
-        'response' => $response,
-    );
-}
-
-function getReplayNonce($acme_res, $http_response_header)
-{
-    preg_match('/^[Rr]eplay-[Nn]once: (.*?)\r\n/sm',
-        $http_response_header, $matches);
-    if (isset($matches[1]) === false) {
-        echo "curl failed: replay nonce header is missing\n";
-        return false;
-    }
-    $acme_res['nonce'] = $matches[1];
-
-    return true;
-}
-
-function signedHttpRequest($acme_res, $url, $payload)
-{
-    // get first nonce
-    if ($acme_res['nonce'] == '') {
-        $ret = httpRequest($acme_res['new_nonce'], 'head');
-        if ($ret === false) {
-            return false;
-        }
-        if (getReplayNonce($acme_res, $ret['header']) === false) {
-            return false;
-        }
-    }
-}
-
-function buildAcmeResource($account_key)
-{
-    $ret = httpRequest(Config::$acme_url_base.'/directory', 'get');
-    if ($ret === false) {
-        return false;
-    }
-
-    $response = json_decode($ret['response'], true);
-    if ($response === false) {
-        echo 'acme/directory failed: invalid response'."\n";
-        return false;
-    }
-
-    $acme_res = array();
-
-    if (isset($response['newNonce']) === false) {
-        echo 'acme/directory failed: `newNonce` not found'."\n";
-        return false;
-    }
-    $acme_res['new_nonce'] = $response['newNonce'];
-
-    if (isset($response['newAccount']) === false) {
-        echo 'acme/directory failed: `newAccount` not found'."\n";
-        return false;
-    }
-    $acme_res['new_account'] = $response['newAccount'];
-
-    if (isset($response['newOrder']) === false) {
-        echo 'acme/directory failed: `newOrder` not found'."\n";
-        return false;
-    }
-    $acme_res['new_order'] = $response['newOrder'];
-
-    if (isset($response['meta']) === false ||
-        isset($response['meta']['termsOfService']) === false) {
-        echo 'acme/directory failed: `meta/termsOfService` not found'."\n";
-        return false;
-    }
-    $acme_res['tos'] = $response['meta']['termsOfService'];
-    $acme_res['nonce'] = '';
-
-    $account_key_info = getRsaKeyInfo($account_key);
-    if ($account_key_info === false) {
-        return false;
-    }
-    $acme_res['account_key_info'] = $account_key_info;
-
-    return $acme_res;
-}
-
-function registerAccount($acme_res)
-{
-    // register account
-    $ret = signedHttpRequest($acme_res, $acme_res['new_account'], array(
-        'termsOfServiceAgreed' => true,
-    ));
-
-    return true;
 }
 
 function main($argc, $argv)
@@ -234,33 +257,27 @@ function main($argc, $argv)
     $tos = isset($cmd_options['t']) ? $cmd_options['t'] : '';
 
     // load account key
-    $account_key = loadAccountKey($account_key_file);
+    $account_key = Util::loadAccountKey($account_key_file);
     if ($account_key === false) {
         return false;
     }
-
     // load csr file
-    $csr = loadCsrFile($csr_file);
+    $csr = Util::loadCsrFile($csr_file);
     if ($csr === false) {
         return false;
     }
 
-    // build acme resource
-    $acme_res = buildAcmeResource($account_key);
-    if ($acme_res === false) {
+    // create acme client
+    $acme_client = new AcmeClient();
+    if ($acme_client->init($account_key) === false) {
         return false;
     }
-
     // check tos
-    if ($tos != '' && $tos != $acme_res['tos']) {
-        echo "terms of service has changed: ".
-             "please modify your -t command option\n".
-             'new tos: '.$acme_res['tos']."\n";
+    if ($acme_client->checkTermOfService($tos) === false) {
         return false;
     }
-
     // register account
-    if (registerAccount($acme_res) === false) {
+    if ($acme_client->registerAccount() === false) {
         return false;
     }
 
